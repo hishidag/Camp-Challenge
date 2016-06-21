6a42882aea52c5ff87e8ac9bd80acda1028fbfe9
hishidag <hishidag@gmail.com>
2016-06-21 13:49:49 +0900

    オブジェクト指向課題４（課題３のクラスを継承する）の解答です

---
+++ ObjectOriented/ObjectExtend.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objectoriented1;
+import objectoriented1.ObjectLesson;
+/**
+ *
+ * @author user1
+ */
+public class ObjectExtend  extends ObjectLesson{
+    
+    public void clearInfo(){
+        this.num = 0;
+        this.str = "";
+    }
+}
